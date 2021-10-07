82c203425197da0c9ae8d0b9ef272b36f665832a
toi0621 <burningup24@gmail.com>
2021-10-07 04:43:57 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' =>'homes#about'
+  get '/search', to: 'searchs#search'
   resources :books do
     resource :favorites, only:[:create, :destroy]
     resources :book_comments, only:[:create, :destroy]
