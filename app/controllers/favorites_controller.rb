db7565dd91154ded0be9104c5826ee3c44edf7c5
toi0621 <burningup24@gmail.com>
2021-10-11 07:51:26 +0000

    Initial Commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,15 @@ class FavoritesController < ApplicationController
 
  def create
   @favorite = current_user.favorites.create(book_id: params[:book_id])
-  redirect_back(fallback_location: root_path)
-
+  @book = Book.find(params[:book_id])
+  
  end
 
  def destroy
   @book = Book.find(params[:book_id])
   @favorite = current_user.favorites.find_by(book_id: @book.id)
   @favorite.destroy
-  redirect_back(fallback_location: root_path)
+  
 
  end
 
